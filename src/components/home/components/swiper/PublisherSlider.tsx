e9bd3f35b42e1d8f6744545aaa8bd5672515b56e
alirezaamb <ababaei78@gmail.com>
2024-06-21 11:44:55 +0330

    style:hide scroll bar of the swiper

--- src/components/home/components/swiper/PublisherSlider.tsx
+++ src/components/home/components/swiper/PublisherSlider.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import {
   A11y,
   Navigation,
   Pagination,
   Scrollbar,
   Autoplay,
-} from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import LoadingPage from "@/components/shared/loading/Loading";
-import { Box, Typography } from "@mui/material";
-import { Publisher } from "@/types/types";
-import { useGetPublisher } from "../../hooks";
+} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import LoadingPage from '@/components/shared/loading/Loading';
+import { Box, Typography } from '@mui/material';
+import { Publisher } from '@/types/types';
+import { useGetPublisher } from '../../hooks';
 
 export const PublisherSlider = () => {
   const { data: publisherPhotos } = useGetPublisher();
@@ -29,15 +29,16 @@ export const PublisherSlider = () => {
       navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
+      className="custom-swiper"
     >
       {!!publisherPhotos && publisherPhotos?.length > 0 ? (
         publisherPhotos?.map((publisher: Publisher) => (
           <SwiperSlide key={publisher.id}>
             <Box
               sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
                 pb: 8,
               }}
             >
@@ -45,14 +46,13 @@ export const PublisherSlider = () => {
                 sx={{
                   height: 142,
                   width: 142,
-                  borderRadius: "50%",
-                  boxShadow: "0 .25rem .75rem rgba(0,0,0,.05)",
+                  borderRadius: '50%',
+                  boxShadow: '0 .25rem .75rem rgba(0,0,0,.05)',
                   backgroundImage: `url(${publisher.image})`,
-                  backgroundSize: "cover",
-                  mb:2
+                  backgroundSize: 'cover',
+                  mb: 2,
                 }}
-              >
-              </Box>
+              ></Box>
               <Typography variant="body1">{publisher.name}</Typography>
             </Box>
           </SwiperSlide>
